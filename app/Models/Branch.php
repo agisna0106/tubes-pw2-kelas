34b4b05475eed5a407ba4a49fc096e3f97bf9c6a
agis16 <sarifan176@gmail.com>
2026-06-01 09:06:23 +0700

    add: relation user and branch

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Branch extends Model
 {
-    //
+    public function users() {
+        return $this->hasMany(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -31,4 +31,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function branch()
+    {
+        return $this->belongsTo(Branch::class);
+    }
 }
